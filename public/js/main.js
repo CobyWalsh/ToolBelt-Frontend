50f92560cc4cffbb85c4f6ebc1b9564c59a12522
Coby Walsh <cobywalsh7@yahoo.com>
2025-05-14 12:27:25 -0600

    added drop down for mobile devices and added a search bar

---
+++ public/js/main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("nav-toggle");
+    const links = document.getElementById("nav-links");
+
+    toggle.addEventListener("click", () => {
+        links.classList.toggle("active");
+    });
+});
