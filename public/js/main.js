b9b3837bfa383ce5d143f04eab9c246268aed851
Coby Walsh <cobywalsh7@yahoo.com>
2025-05-14 14:59:50 -0600

    started to implement toolbelt carousel

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,5 @@
+import * as THREE from 'three';
+
 document.addEventListener("DOMContentLoaded", () => {
     const toggle = document.getElementById("nav-toggle");
     const links = document.getElementById("nav-links");
@@ -6,3 +8,24 @@ document.addEventListener("DOMContentLoaded", () => {
         links.classList.toggle("active");
     });
 });
+
+let scene, camera, renderer, beltGroup;
+
+function init() {
+    // set up scene
+    scene = new THREE.Scene();
+
+    // set up camera
+    const fov = 75;
+    const aspect = window.innerWidth / window.innerHeight;
+    const near = 0.1;
+    const far = 1000;
+    camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+    camera.position.z = 5;
+
+    // set up renderer
+    renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true});
+    renderer.setSize(window.innerWidth, window.innerHeight);
+
+    document.getElementById('carousel-container').appendChild(renderer.domElement);
+}
